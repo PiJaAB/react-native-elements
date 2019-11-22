48b1cccb3472d34f9c88cf223d606d448c740eae
Ylva <ylva.rehnberg@pija.se>
2019-11-22 18:43:18 +0100

    fix get primary font family for form input

--- Elements/FormInput/createFormInputStyles.js
+++ Elements/FormInput/createFormInputStyles.js
@@ -1,12 +1,16 @@
 // @flow
-import { type ThemeSpec } from '../../Theme';
+import type ThemeSpec from '../../Theme';
 
-export default ({ colors }: ThemeSpec) => ({
+export default ({ colors, dimensions, fonts, font }: ThemeSpec) => ({
   textInput: {
     width: '100%',
     paddingBottom: 13,
     color: colors.primary,
     borderBottomWidth: 2,
     borderColor: colors.primaryBorder,
+    ...font({
+      fontFamily: fonts.primary.fontFamily,
+      fontWeight: '300',
+    }),
   },
 });
