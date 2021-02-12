34c30228ecd717298ab18fcb3f4f884821d05f33
markusekblad <markus.ekblad@pija.se>
2021-02-12 12:05:11 +0100

    Expose the theme context, update to version 0.1.21

--- Theme/index.js
+++ Theme/index.js
@@ -4,6 +4,7 @@ import ThemeContext from './ThemeContext';
 export type { ThemeSpec } from './Theme';
 
 export const ThemeProvider = ThemeContext.Provider;
+export { default as Context } from './ThemeContext';
 export { default as Theme } from './Theme';
 export { default as withTheme } from './withTheme';
 export * from './utilities';
